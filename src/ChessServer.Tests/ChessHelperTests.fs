92582051a4eda3b3290f98409e064c3c0e947b6b
alex_r <shorks@ya.ru>
2019-07-26 19:59:03 +0300

    WIP tests

---
+++ src/ChessServer.Tests/ChessHelperTests.fs
@@ -0,0 +1,94 @@
+﻿module ChessHelperTests
+
+open System
+open Xunit
+open ChessServer
+
+open ChessHelper
+
+let throwsAny f = Assert.ThrowsAny(fun () -> f() |> ignore)
+
+[<Fact>]
+let ``getColumn a returns 0`` () =
+    let col = getColumn "a5"
+    Assert.Equal(0uy, col)
+
+[<Fact>]
+let ``getColumn h returns 7`` () =
+    let col = getColumn "h5"
+    Assert.Equal(7uy, col)
+
+[<Fact>]
+let ``getColumn second symbol ignored`` () =
+    let col = getColumn "a!"
+    Assert.Equal(0uy, col)
+
+[<Fact>]
+let ``getColumn not in range a-h throws`` () =
+    throwsAny(fun () -> getColumn "z1") |> ignore
+    throwsAny(fun () -> getColumn "i1") |> ignore
+    throwsAny(fun () -> getColumn "51") |> ignore
+    throwsAny(fun () -> getColumn "!1") |> ignore
+    0
+
+[<Fact>]
+let ``getColumn null throws`` () =
+    let ex = throwsAny(fun () -> getColumn null)
+    Assert.Equal(ex.GetType(), typeof<ArgumentNullException>)
+
+[<Fact>]
+let ``getColumn invalid symbol count`` () =
+    throwsAny(fun () -> getColumn "a") |> ignore
+    throwsAny(fun () -> getColumn "1") |> ignore
+    throwsAny(fun () -> getColumn "1bx") |> ignore
+
+
+
+
+
+[<Fact>]
+let ``geRow 1 returns 7`` () =
+    let row = getRow "a1"
+    Assert.Equal(7uy, row)
+
+[<Fact>]
+let ``getRow 8 returns 0`` () =
+    let col = getRow "h8"
+    Assert.Equal(0uy, col)
+
+[<Fact>]
+let ``getRow first symbol ignored`` () =
+    let input = "!5"
+    let col = getRow input
+    Assert.Equal(3uy, col)
+
+[<Fact>]
+let ``getRow not in range 1-8 throws`` () =
+    Assert.ThrowsAny(fun () -> getRow "z0" |> ignore) |> ignore
+    Assert.ThrowsAny(fun () -> getRow "i9" |> ignore) |> ignore
+    Assert.ThrowsAny(fun () -> getRow "x!" |> ignore) |> ignore
+    0
+
+[<Fact>]
+let ``getRow null throws`` () =
+    let ex = Assert.ThrowsAny(fun () -> getRow null |> ignore)
+    Assert.Equal(ex.GetType(), typeof<ArgumentNullException>)
+
+[<Fact>]
+let ``getRow invalid symbol count`` () =
+    Assert.ThrowsAny(fun () -> getRow "a" |> ignore) |> ignore
+    Assert.ThrowsAny(fun () -> getRow "1" |> ignore) |> ignore
+    Assert.ThrowsAny(fun () -> getRow "1bx" |> ignore) |> ignore
+
+
+
+
+[<Fact>]
+let ``getPosition 0 returns a8`` () =
+    let result = getPosition 0uy
+    Assert.Equal(result, "a8")
+
+[<Fact>]
+let ``getPosition check range`` () =
+    Assert.ThrowsAny(fun () -> getPosition 100uy)
+    Assert.Equal(result, "a8")
--- src/ChessServer/ChessHelper.fs
+++ src/ChessServer/ChessHelper.fs
@@ -14,7 +14,7 @@ module ChessHelper =
         | 2 ->
             let col = input.Substring(0, 1).ToLowerInvariant().[0]
             if col < 'a' || col > 'h' then
-                failwithf "Invalid argument '%s' - column not in range" input
+                failwithf "Invalid argument '%s' - column not in range (a-h)" input
             
             byte col - byte 'a'
         | _ -> failwithf "Invalid input string length '%s'" input
@@ -24,14 +24,21 @@ module ChessHelper =
             raise (ArgumentNullException("input"))
 
         match input.Length with
-        | 2 -> 8 - (input.Substring(1, 1) |> Int32.Parse) |> byte
+        | 2 -> 
+            let row = input.Substring(1, 1).ToLowerInvariant() |> int
+            if row < 1 || row > 8 then
+                failwithf "Invalid argument '%s' - row not in range (1-8)" input
+            8 - (input.Substring(1, 1) |> Int32.Parse) |> byte
         | _ -> failwithf "Invalid input string length '%s'" input
 
     let getPosition (pos: byte) =
+        if pos > 64uy then 
+            failwithf "Invalid argument: '%d' not in range (1-64)" pos
+        
         let pos = int pos
         let col = pos % 8
         let row = pos / 8
-        ((int 'a' + 7 - col) |> char |> string) + (row + 1).ToString()
+        ((int 'a' + col) |> char |> string) + (8 - row).ToString()
         
     let moveAction from _to = {From = from; To = _to}
 
