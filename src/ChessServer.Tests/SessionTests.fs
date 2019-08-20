ce6ad5acb9f655fc05dfbb1f4604fd4771a36bd6
alex_r <shorks@ya.ru>
2019-08-20 08:16:14 +0300

    Update tests

--- src/ChessServer.Tests/SessionTests.fs
+++ src/ChessServer.Tests/SessionTests.fs
@@ -3,6 +3,7 @@
 open Session
 open Types.Channel
 open FsUnit.Xunit
+open SessionTypes
 open Types.Command
 open Xunit
 open TestHelper
@@ -26,98 +27,154 @@ let createStateAgent state = MailboxProcessor<StateAgentMessage<'a>>.Start(fun i
 )
 
 let notifyStub = TestNotify {Message=""}
+let moveStub = {
+    From = ""
+    To = "" 
+    PawnPromotion = None
+}
+
+type TestChannel = {
+    Id: string
+    Channel: ClientChannel
+}
+
+type TestChannels = {
+    White: TestChannel
+    Black: TestChannel
+    CreateSession: unit -> Session * Session
+    WhiteNotify: unit -> Notify
+    BlackNotify: unit -> Notify
+    WhiteState: unit -> ClientState
+    BlackState: unit -> ClientState
+    Reset: unit -> unit
+}
 
 let channelInfo () =
-    let notifyAgent = createStateAgent notifyStub
-    let stateAgent = createStateAgent New
-    let channel = {
-        Id = ""
-        PushNotification = fun n -> notifyAgent.Post <| SetState n 
-        ChangeState = fun s -> 
-            stateAgent.Post <| SetState s
+    let createChannel() =
+        let notifyAgent = createStateAgent notifyStub
+        let stateAgent = createStateAgent New
+        let channel = {
+            Id = ""
+            PushNotification = fun n -> notifyAgent.Post <| SetState n 
+            ChangeState = fun s -> 
+                stateAgent.Post <| SetState s
+        }
+        let checkNotify() = notifyAgent.PostAndReply GetState
+        let checkState() = 
+            let x = stateAgent.PostAndReply GetState
+            x
+
+        let reset() =
+            notifyAgent.Post <| SetState notifyStub
+            stateAgent.Post <| SetState New 
+
+        channel, checkNotify, checkState, reset
+        
+    let wChannel, wCheckNotify, wCheckState, wReset = createChannel()
+    let bChannel, bCheckNotify, bCheckState, bReset = createChannel()
+    let whiteId = "w"
+    let blackId = "b"
+    
+    {
+        White = {
+            Id = whiteId
+            Channel = wChannel
+        }
+        Black = {
+            Id = blackId
+            Channel = bChannel
+        }
+        WhiteNotify = wCheckNotify
+        BlackNotify = bCheckNotify
+        WhiteState = wCheckState
+        BlackState = bCheckState
+        Reset = wReset >> bReset
+        CreateSession = fun () -> createSession wChannel bChannel
     }
-    let checkNotify() = notifyAgent.PostAndReply GetState
-    let checkState() = 
-        let x = stateAgent.PostAndReply GetState
-        x
-
-    let reset() =
-        notifyAgent.Post <| SetState notifyStub
-        stateAgent.Post <| SetState New 
-
-    channel, checkNotify, checkState, reset
 
 
 [<Fact>]
 let ``chat check notification`` () =
-    let whiteChannel, whiteNotify, _, _ = channelInfo()
-    let blackChannel, blackNotify, _, _ = channelInfo()
+    let channels = channelInfo()
 
-    let ({ChatMessage = whiteChat}, {ChatMessage = blackChat}) = createSession whiteChannel blackChannel
-
-    whiteChat "w"
-    blackChat "b"
+    let ({ChatMessage = whiteChat}, {ChatMessage = blackChat}) = channels.CreateSession()
 
     let checkNotify notify msg = 
         match notify() with
         | ChatNotify n -> n.Message |> should equal msg
         | _ -> failTest "not a chat notify"
+        
+    let checkEmptyNotify notify =
+        if notify() <> notifyStub then failTest "should be empty notify"
 
-    checkNotify whiteNotify "b"
-    checkNotify blackNotify "w"
+    whiteChat "w"
+    checkNotify channels.BlackNotify "w"
+    checkEmptyNotify channels.WhiteNotify
+    
+    channels.Reset()
+    
+    blackChat "b"
+    checkNotify channels.WhiteNotify "b"
+    checkEmptyNotify channels.BlackNotify
 
 [<Fact>]
 let ``close session check notification`` () =
-    let checkInternal session notify msg = 
-        let notifyMsg notify = 
+    let checkInternal session notify1 notify2 msg = 
+        let closeNotifyMsg notify = 
             match notify with
             | SessionCloseNotify {Message = msg} -> msg
             | _ -> failTest "not a session close notify"
             
         session.CloseSession msg
-        notify() |> notifyMsg |> should haveSubstring msg
+        notify1() |> closeNotifyMsg |> should haveSubstring msg
+        notify2() |> should equal notifyStub
     
-    let whiteChannel, whiteNotify, _, _ = channelInfo()
-    let blackChannel, blackNotify, _, _ = channelInfo()
-    let createSession() = createSession whiteChannel blackChannel
+    let channels = channelInfo()
 
-    let sw, _ = createSession()
-    checkInternal sw blackNotify "white"
+    let sw, _ = channels.CreateSession()
+    checkInternal sw channels.BlackNotify channels.WhiteNotify "white"
     
-    let _, sb = createSession()
-    checkInternal sb whiteNotify "black"
+    channels.Reset()
+    
+    let _, sb = channels.CreateSession()
+    checkInternal sb channels.WhiteNotify channels.BlackNotify "black"
     
 [<Fact>]
 let ``close session check new state`` () =
     // setup
-    let whiteChannel, _, whiteState, _ = channelInfo()
-    let blackChannel, _, blackState, _ = channelInfo()
-    let createSession() = createSession whiteChannel blackChannel
-    let resetStates() = 
-        [whiteChannel.ChangeState; blackChannel.ChangeState]
-        |> applyMany Matching
+    let channels = channelInfo()
     let call f = f()
-    let checkStates() = [whiteState; blackState] |> List.iter (call >> should equal New)
+    let checkStates() = [channels.WhiteState; channels.BlackState] |> List.iter (call >> should equal New)
 
     let testAction session = 
-        resetStates()
+        channels.Reset()
         session.CloseSession "test"
         checkStates()
 
     // assert
     // white
-    let sw, _ = createSession()
+    let sw, _ = channels.CreateSession()
     testAction sw
 
     //black
-    let _, sb = createSession()
+    let _, sb = channels.CreateSession()
     testAction sb
 
-let 
-    //s1.ChatMessage "gg"
-    //s1.CreateMove {
-    //    From = "a2"
-    //    To = "a4" 
-    //    PawnPromotion = None
-    //} |> ignore
-    //()
+[<Fact>]
+let ``close session check exception on any function calls`` () =
+    let sessionError = typeof<SessionException>
+    let channels = channelInfo()
+    let checkAnyFunctionThrows s =
+        (fun () -> s.CloseSession "test") |> should throw sessionError
+        (fun () -> s.ChatMessage "test") |> should throw sessionError
+        (fun () -> s.CreateMove moveStub) |> should throw sessionError
+    
+    let sw, sb = channels.CreateSession()
+    sw.CloseSession "white"
+    checkAnyFunctionThrows sw
+    checkAnyFunctionThrows sb
+    
+    let sw, sb = channels.CreateSession()
+    sb.CloseSession "black"
+    checkAnyFunctionThrows sw
+    checkAnyFunctionThrows sb
